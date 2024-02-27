9d40f948976d4b4caf8bbba30504630cef9a1328
Thi <tthiagovinnicius@gmail.com>
2024-02-27 00:47:28 -0300

    tratamento de erros simples

--- api/app.py
+++ api/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return redirect('/')
 
 
 if __name__ == '__main__':
