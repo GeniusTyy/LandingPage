66609bf0a5bed048e7fbb95bab0422e3fec8847b
ParsivalT <tthiagovinnicius@gmail.com>
2024-02-26 21:23:45 -0300

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -3,6 +3,6 @@ from config import PATH, PATH_DB
 from flask import Flask
 
 app = Flask(__name__)
-app.secret_key = 'gjfakkjfasijgçalkgk'
+app.secret_key = 'gjfakkjfasijgccalkgk'
 
 from core import routes 
