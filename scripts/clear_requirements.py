37242e812cad6de21a3af01f1f4ec2b78c3b28e8
Thi <tthiagovinnicius@gmail.com>
2024-03-01 00:10:38 -0300

    integrando o  ao projeto para formatação

--- scripts/clear_requirements.py
+++ scripts/clear_requirements.py
@@ -1,20 +1,22 @@
 """
-    Proposito: 
-        Script para auxiliar a criação do 'requirements.txt' com o 'Poetry'.
-        Ao rodar o 'Poetry export --without-hashs' para exportar o 'requirements.txt',
-        ele vem com informações que não são necessárias. Este script remove essas informações desnecessárias.
+Proposito:
+    Script para auxiliar a criação do 'requirements.txt' com o 'Poetry'.
+    Ao rodar o 'Poetry export --without-hashs' para exportar o 'requirements.txt',
+    ele vem com informações que não são necessárias. Este script remove essas informações desnecessárias.
 """
+
 import re
 
+
 # Abre o arquivo 'requirements.txt' no modo de leitura e escrita ('r+').
-with open('requirements.txt', 'r+') as file:
+with open("requirements.txt", "r+") as file:
     # Lê todas as linhas do arquivo 'requirements.txt' e as armazena na variável 'lines'.
     lines = file.readlines()
 
     # Utiliza uma compreensão de lista para remover tudo após o caractere ';' em cada linha.
     # A função 're.sub' substitui a parte da string que corresponde à expressão regular por uma string vazia.
     # O método 'strip' é usado para remover espaços em branco no início e no fim de cada linha.
-    data = [re.sub(r';.*', '', line).strip() for line in lines]
+    data = [re.sub(r";.*", "", line).strip() for line in lines]
 
     # Move o ponteiro do arquivo para o início, efetivamente apagando o conteúdo anterior.
     file.seek(0)
@@ -23,4 +25,4 @@ with open('requirements.txt', 'r+') as file:
 
     # Escreve as linhas modificadas de volta ao arquivo 'requirements.txt', adicionando uma nova linha após cada uma.
     for line in data:
-        file.write(line + '\n')
+        file.write(line + "\n")
