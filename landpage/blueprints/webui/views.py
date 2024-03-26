517e115111b435572682d0dfa021f42063ba80c2
ParsivalT <tthiagovinnicius@gmail.com>
2024-03-25 21:43:14 -0300

    feat: salvando os dados no PostgreSQL

--- landpage/blueprints/webui/views.py
+++ landpage/blueprints/webui/views.py
@@ -1,10 +1,36 @@
-from flask import render_template, flash, current_app
+from flask import render_template, flash, current_app, request
+from datetime import datetime
+
+from landpage.models import User
+from landpage.ext.database import db
 
 
 # Define uma função para renderizar a página inicial
 def home():
+    if request.method == "POST":
 
-    # Retorna uma string HTML que será renderizada como a página inicial
-    flash("Esta é uma mensagem de teste", "success")
+        # Pegando o email informado no formulario.
+        email = request.form.get("email")
+
+        # Formata a data corretamente para o formato 'YYYY-MM-DD'
+        data = datetime.now().strftime("%Y-%m-%d")
+        try:
+
+            # Cria um novo objeto User
+            user = User(email=email, date=data)
+
+            # Adiciona o novo usuário à sessão do banco de dados
+            db.session.add(user)
+
+            # Confirma as mudanças no banco de dados
+            db.session.commit()
+
+            flash("Inscrição realizada com Sucesso!", "sucess")
+        except Exception as err:
+            flash(
+                "Ocorreu um erro ao processar sua solicitação. Por favor, tente novamente mais tarde.",
+                "error",
+            )
+            pass
 
     return render_template("home.html")
--- landpage/models.py
+++ landpage/models.py
@@ -1,6 +1,8 @@
 from landpage.ext.database import db
 from sqlalchemy_serializer import SerializerMixin
 
+
 class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(140))
+    date = db.Column(db.Date)
